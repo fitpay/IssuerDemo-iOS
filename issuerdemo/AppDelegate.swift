c8795b532b0e6d69301532ff8a677a8df8bceca3
Jeremiah Harris <jeremiah@fit-pay.com>
2018-04-27 12:30:57 -0600

    Initial Commit

--- issuerdemo/AppDelegate.swift
+++ issuerdemo/AppDelegate.swift
@@ -1,46 +1,45 @@
-//
-//  AppDelegate.swift
-//  issuerdemo
-//
-//  Created by Jeremiah Harris on 4/27/18.
-//  Copyright © 2018 Fitpay. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    var payload: String?
+    var wpCallback: String?
+    var useAuthCode = true
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UIApplication.shared.statusBarStyle = .lightContent
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
+        payload = url.queryParameters?["a2apayload"]
+        wpCallback = url.queryParameters?["wpcallback"]
+        
+        if (url.absoluteString.starts(with: "pagare://a2a_generate_auth_code")) {
+            useAuthCode = true
+        } else if (url.absoluteString.starts(with: "pagare://a2a_perform_auth")) {
+            useAuthCode = false
+        }
+        
+        return true
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
+extension URL {
+    
+    public var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true), let queryItems = components.queryItems else { return nil }
+        
+        var parameters = [String: String]()
+        for item in queryItems {
+            parameters[item.name] = item.value
+        }
+        
+        return parameters
+    }
+}
---
+++ issuerdemo/SimulatedAppToAppViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+class SimulatedAppToAppViewController: UIViewController {
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        passwordTF.isHidden = !appDelegate.useAuthCode
+    }
+    
+    @IBAction func confirmPassword(_ sender: Any) {
+        if passwordTF?.text?.isEmpty == true && appDelegate.useAuthCode {
+            showError("No Password", message: "Password is empty")
+            return
+        }
+        
+        guard let wpCallback = appDelegate.wpCallback else {
+            showError("No callback", message: "Launch app with wpcallback parameter")
+            return
+        }
+        
+        var authResponse = "approved"
+        
+        switch (passwordTF.text?.last) {
+        case "d":
+            authResponse = "declined"
+            break;
+        case "f":
+            authResponse = "failure"
+            break
+        default:
+            break
+        }
+        
+        var urlString = "\(wpCallback)?stepupresponse=\(authResponse)"
+        if (appDelegate.useAuthCode) {
+            urlString.append("&stepupauthcode=\(passwordTF!.text!)")
+        }
+        
+        let url = URL(string: urlString)!
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        
+        passwordTF.text = ""
+        passwordTF.resignFirstResponder()
+    }
+    
+    func showError(_ title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        self.present(alertController, animated: true) {}
+    }
+    
+}
+
+extension SimulatedAppToAppViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        passwordTF.resignFirstResponder()
+        return true
+    }
+    
+}
